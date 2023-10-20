65966543bd4af523778f674c4f7292bccb1aadd9
Sergey Erofeev <erofsey@yandex.kz>
2023-10-20 20:26:47 +0600

    add Response

---
+++ src/core/Response.js
@@ -0,0 +1,43 @@
+export default class Response {
+  static _initialization = false
+
+  _options = {}
+
+  _baseUrl = ''
+
+  constructor() {
+    if (!Response._initialization) {
+      throw new TypeError('Нельзя напрямую создать экземпляр данного класса')
+    }
+  }
+
+  static create(options = {}) {
+    Response._initialization = true
+    const response = new Response()
+    Response._initialization = false
+
+    response._baseUrl = options.baseUrl
+    delete options.baseUrl
+    response._options = options
+
+    return response
+  }
+
+  _response = async (url, options = {}) => {
+    try {
+      const response = await fetch(url, options)
+
+      return await response.json()
+    } catch (e) {
+      // eslint-disable-next-line
+      if (process.env.NODE_ENV === 'development') console.log(e.message)
+      throw e
+    }
+  }
+
+  get(url = '') {
+    url = new URL(url, this._baseUrl)
+
+    return this._response(url.href, this._options)
+  }
+}
