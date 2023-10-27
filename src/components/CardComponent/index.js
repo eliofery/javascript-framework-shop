15628308222ff6064ee81d4b6c5e3caf6af3d9a6
Sergey Erofeev <erofsey@yandex.kz>
2023-10-27 13:07:42 +0600

    add CardComponent

--- src/components/CardComponent/index.js
+++ src/components/CardComponent/index.js
@@ -1,43 +1,78 @@
 import BaseComponent from '@/core/BaseComponent'
-import '@/components/CardComponent/card.scss'
+import { priceFormat } from '@/utils/num'
+
+import '@/components/CardComponent/product-card.scss'
 
 export default class CardComponent extends BaseComponent {
-  constructor({ title = '', price = 0, description = '' } = {}) {
-    super ()
+  constructor(data = {}) {
+    super()
 
-    this.title = title
-    this.price = price
-    this.description = description
+    this._id = data.id
+    this._scu = data.scu
+    this._title = data.title
+    this._complexName = data.complex_name
+    this._square = data.square
+    this._priceSqM = data.price_sq_m
+    this._priceTotal = data.price_total
+    // this._building = data['building']
+    this._floor = data.floor
+    this._floorsTotal = data.floors_total
+    this._rooms = data.rooms
+    // this._flatNumber = data['flat_number']
+    this._image = data.image
 
     this._init()
   }
 
+  async _loadData() {
+    //
+  }
+
+  async _updateData() {
+    //
+  }
+
   get _template() {
     return `
-      <div>
-        <h1 data-el="title">${this.title}</h1>
-        <div data-el="description">${this.description}</div>
-        <div data-el="price">Price: ${this.price}</div>
-
-        <button type="button" data-el="btn1">Add</button>
-        <button type="button" data-el="btn2">Remove</button>
-      </div>
+      <article class="product-card">
+        <a class="product-card__link" href="/item/${this._id}">
+          <header class="product-card__header">
+            <h2 class="product-card__title">ЖК ${this._complexName}</h2>
+            <span class="product-card__favorite" data-el="favorite" tabindex="0">
+              <svg width="24" height="24"><use xlink:href="#icon-heart"></use></svg>
+            </span>
+          </header>
+          <p class="product-card__pic">
+            <img class="product-card__img" width="300" height="300" src="${
+              this._image
+            }" alt="ЖК ${this._complexName}">
+          </p>
+          <div class="product-card__desc">
+            <p class="product-card__price">
+              <b class="product-card__price-total">${priceFormat(
+                this._priceTotal,
+              )} ₸</b>
+              <span class="product-card__price-square">${priceFormat(
+                this._priceSqM,
+              )} ₸/м<sup>2</sup></span>
+            </p>
+            <dl class="product-card__param">
+              <dt class="product-card__param-title">Комнат</dt>
+              <dd class="product-card__param-desc">${this._rooms}</dd>
+              <dt class="product-card__param-title">Площадь</dt>
+              <dd class="product-card__param-desc">${this._square}</dd>
+            </dl>
+          </div>
+          <footer class="product-card__footer">
+            <span class="product-card__scu">${this._scu}</span>
+            <span class="product-card__floor">Этаж ${this._floor} из ${
+              this._floorsTotal
+            }</span>
+          </footer>
+        </a>
+      </article>
     `
   }
 
-  _initListeners() {
-    const { btn1 } = this._elements
-
-    btn1.addEventListener('click', () => {
-      console.log('btn1')
-      this.destroy()
-    })
-
-    // document.addEventListener('click', () => {
-    //   console.log('document')
-    //   this.destroy()
-    // }, {
-    //   signal: this._abortController.signal,
-    // })
-  }
+  _initListeners() {}
 }
